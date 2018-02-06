8f9caee498f0858397b86ef519e936d20e51e098
cyber01 <tnptnpj37@gmail.com>
2018-02-05 21:05:50 -0800

    Finish 6feb

--- app/src/main/java/masterung/androidthai/in/th/recordweight/MainActivity.java
+++ app/src/main/java/masterung/androidthai/in/th/recordweight/MainActivity.java
@@ -1,9 +1,11 @@
 package masterung.androidthai.in.th.recordweight;
 
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import masterung.androidthai.in.th.recordweight.utility.MyManager;
+import masterung.androidthai.in.th.recordweight.utility.MyOpenHelperSQLite;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,11 +19,25 @@ public class MainActivity extends AppCompatActivity {
         myManager = new MyManager(MainActivity.this);
 
 //        Test Add User
-        myManager.addValueToUserTABLE("nick" , "start" ,
-                "weight" , "height" , "gender" ,
-                "age");
+//        testAddUser();
+
+
+//        Delete All userTABLE
+        deleteAllUserTABLE();
 
 
     }   // Main Method
+
+    private void deleteAllUserTABLE() {
+        SQLiteDatabase sqLiteDatabase = openOrCreateDatabase(MyOpenHelperSQLite.databaseName ,
+                MODE_PRIVATE , null);
+        sqLiteDatabase.delete("userTABLE" , null , null);
+    }
+
+    private void testAddUser() {
+        myManager.addValueToUserTABLE("nick" , "start" ,
+                "weight" , "height" , "gender" ,
+                "age");
+    }
 
 }   // Main Class
